48aead5f07df01efb5224dcadeb78a9bc566ae97
Minhyun <alsgus3074@gmail.com>
2024-09-06 17:44:56 +0900

    todos

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const fs = require('fs');
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const todos = require('./api/todos');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+
+app.use('/todos',todos);
+
+app.listen(4000)
